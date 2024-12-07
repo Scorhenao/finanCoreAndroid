b148dc7be01f212f50b2fa6947f1cf9a7f746a73
Scorhenao <samcorhenao@gmail.com>
2024-12-06 20:18:00 -0500

    fix: kic manifiest to use the camera with image picker

--- src/components/CircleImage.tsx
+++ src/components/CircleImage.tsx
@@ -1,24 +1,80 @@
-import React from 'react';
-import {Image, StyleSheet, View} from 'react-native';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  Image,
+  Alert,
+} from 'react-native';
+import {launchCamera} from 'react-native-image-picker';
+import {launchImageLibrary} from 'react-native-image-picker';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {useTheme} from '../context/ThemeContext';
+import {useState} from 'react';
 
 export const CircleImage = () => {
+  const [imageUri, setImageUri] = useState('https://i.pravatar.cc/100');
   const {darkMode} = useTheme();
+
+  const handleImageSelect = () => {
+    launchImageLibrary({mediaType: 'photo'}, response => {
+      if (response.assets && response.assets.length > 0) {
+        setImageUri(response.assets[0].uri);
+      }
+    });
+  };
+
+  const handleTakePhoto = () => {
+    launchCamera(
+      {
+        mediaType: 'photo',
+        cameraType: 'back', // Puedes especificar 'front' para la cámara frontal
+        saveToPhotos: true, // Guardar la foto en el álbum del dispositivo
+      },
+      response => {
+        if (response.didCancel) {
+          console.log('User cancelled camera picker');
+        } else if (response.errorCode) {
+          console.log('Error: ', response.errorMessage);
+        } else {
+          setImageUri(response.assets[0].uri); // Establecer la URI de la foto tomada
+        }
+      },
+    );
+  };
+
+  const handleIconPress = () => {
+    // Mostrar un menú para seleccionar entre cámara o galería
+    Alert.alert('Choose an option', 'Select a method to pick an image', [
+      {
+        text: 'Camera',
+        onPress: handleTakePhoto,
+      },
+      {
+        text: 'Gallery',
+        onPress: handleImageSelect,
+      },
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+    ]);
+  };
 
   return (
     <View style={CircleImageStyles.container}>
-      <Image
-        source={{uri: 'https://i.pravatar.cc/100'}}
-        style={CircleImageStyles.container}
-      />
+      <TouchableOpacity onPress={handleIconPress}>
+        <Image source={{uri: imageUri}} style={CircleImageStyles.image} />
+      </TouchableOpacity>
 
-      <Icon
-        style={CircleImageStyles.icon}
-        name="add-circle-outline"
-        size={30}
-        color={darkMode ? '#fff' : '#000'}
-      />
+      <TouchableOpacity onPress={handleIconPress}>
+        <Icon
+          style={[CircleImageStyles.icon, CircleImageStyles.addIcon]}
+          name="add-circle-outline"
+          size={30}
+          color={darkMode ? '#fff' : '#000'}
+        />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -30,10 +86,22 @@ const CircleImageStyles = StyleSheet.create({
     borderRadius: 100 / 2,
     justifyContent: 'center',
     alignItems: 'center',
+    position: 'relative',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    resizeMode: 'cover',
   },
   icon: {
     position: 'absolute',
     bottom: -8,
-    right: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    padding: 3,
+    borderRadius: 20,
+  },
+  addIcon: {
+    left: 20,
   },
 });
